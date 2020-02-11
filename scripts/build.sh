1f6fb441f744a8d53dc781246f8c2c433c88ecc9
Adrosar <adriangargula@interia.pl>
2020-02-11 22:25:10 +0100

    Fixes for WIndows (cmd)

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-PROJECT_DIR="$CURRENT_DIR/.."
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJECT_DIR="$SCRIPTS_DIR/.."
 BUILD_DIR="$PROJECT_DIR/build"
 
 cd "$PROJECT_DIR/cmd/app"
